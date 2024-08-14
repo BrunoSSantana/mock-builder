f4c27a3ae6fef022e52908a56416055b55aab63a
BrunoSSantana <brunoosouza15@gmail.com>
2024-08-14 07:58:34 -0300

    docs: add docstring

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
 export type Shape<T> = {
   [K in keyof T]: T[K] | (() => T[K]);
 };
-
+/**
+ * Builds an object from a shape
+ */
 export class Builder<T> {
   private _shape: Shape<T>;
 
+  /**
+   * ## Creates a new Builder
+   *
+   * @param shape
+   * @returns Builder
+   *
+   * @example
+   * ```ts
+   * const builder = new Builder({
+   *   id: faker.number.int,
+   *   name: faker.lorem.word
+   * });
+   *
+   * ```
+   */
   constructor(shape: Shape<T>) {
     this._shape = shape;
   }
 
+  /**
+   * ## Builds an object
+   *
+   * @returns T
+   *
+   * @example
+   * ```ts
+   * const foo = builder.build();
+   * ```
+   */
   build(): T {
     const data = Object.entries(this._shape).reduce((acc, [key, value]) => {
       if (typeof value === "function") {
